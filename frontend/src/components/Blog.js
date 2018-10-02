a6a69adac84095ed6c5ac842a01871c875fa6c81
kaj_pesio <15047180+KajPe@users.noreply.github.com>
2018-10-03 01:07:30 +0300

    Teht 5.6

--- frontend/src/components/Blog.js
+++ frontend/src/components/Blog.js
@@ -1,9 +1,47 @@
 import React from 'react'
 
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+class Blog extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      visible: false
+    }
+  }
+
+  toggleVisibility = () => {
+    this.setState({ visible: !this.state.visible })
+  }
+
+  render() {
+    const showWhenVisible = { display: this.state.visible ? '' : 'none' }
+
+    const blogStyle = {
+      paddingTop: 10,
+      paddingLeft: 2,
+      border: 'solid',
+      borderWidth: 1,
+      marginBottom: 5
+    }
+
+    const mouseStyle = {
+      cursor: 'pointer'
+    }
+
+    const paddingStyle = {
+      paddingLeft: 20
+    }
+
+    return (
+      <div style={blogStyle}>
+        <div style={mouseStyle} onClick={this.toggleVisibility}>{this.props.blog.title}</div>
+        <div style={{...showWhenVisible,...paddingStyle}}>
+          <div><a href={this.props.blog.url}>{this.props.blog.url}</a></div>
+          <div>{this.props.blog.likes} likes <button>like</button></div>
+          <div>Added by: {this.props.blog.author}</div>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Blog
