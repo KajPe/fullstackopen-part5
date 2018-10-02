3ad75ad989f39f0ab3cab7667f60b1d3e1a66690
kaj_pesio <15047180+KajPe@users.noreply.github.com>
2018-10-03 01:06:02 +0300

    Teht 5.5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import NewBlog from './components/NewBlog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
+import Togglable from './components/Togglable'
 
 class App extends React.Component {
   constructor(props) {
@@ -100,10 +101,8 @@ class App extends React.Component {
   }
 
   render() {
-    const showBlogs = () => (
-      <div>
-        <h2>Blogs</h2>
-        <p>{this.state.user.name} logged in <button onClick={this.logout}>Logout</button></p>
+    const newBlogForm = () => (
+      <Togglable buttonLabel="New Blog" ref={component => this.blogForm = component}>
         <NewBlog 
           title={this.state.title}
           author={this.state.author}
@@ -111,6 +110,14 @@ class App extends React.Component {
           addNewBlog={this.addBLog}
           handleFieldChange={this.handleFieldChange}
         />
+      </Togglable>
+    )
+
+    const showBlogs = () => (
+      <div>
+        <h2>Blogs</h2>
+        <p>{this.state.user.name} logged in <button onClick={this.logout}>Logout</button></p>
+        {newBlogForm()}
         <br />
         {this.state.blogs.map(blog => 
           <Blog key={blog.id} blog={blog}/>
