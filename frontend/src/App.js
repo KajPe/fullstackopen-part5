26c4ea93e05c36c341148e11e681ee869c2dac23
kaj_pesio <15047180+KajPe@users.noreply.github.com>
2018-10-03 00:12:05 +0300

    Teht 5.1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,28 +1,99 @@
 import React from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from './components/Notification'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      blogs: []
+      blogs: [],
+      error: null,
+      username: '',
+      password: '',
+      user: null
     }
   }
 
-  componentDidMount() {
-    blogService.getAll().then(blogs =>
+  async componentDidMount() {
+    try {
+      const blogs = await blogService.getAll()
       this.setState({ blogs })
-    )
-  } 
+    } catch(exception) {
+      this.setState({
+        error: 'Failed to retrieve blogs',
+      })
+    }
+  }
+
+  handleLoginFieldChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  login = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({
+        username: this.state.username,
+        password: this.state.password
+      })
+      this.setState({ username:'', password:'', user:user })
+    } catch(exception) {
+      this.setState({
+        error: 'Bad username or password',
+      })
+      setTimeout(() => {
+        this.setState({ error: null })
+      }, 3000)
+    }
+  }
 
   render() {
+    const showBlogs = () => (
+      <div>
+        <h2>Blogs</h2>
+        <p>{this.state.user.name} logged in</p>
+        {this.state.blogs.map(blog => 
+          <Blog key={blog.id} blog={blog}/>
+        )}
+      </div>
+    )
+  
+    const loginForm = () => (
+      <div>
+        <h2>Login to application</h2>
+        <form onSubmit={this.login}>
+          <div>
+            Username :
+            <input
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <div>
+            Password :
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleLoginFieldChange}
+            />
+          </div>
+          <button type="submit">Login</button>
+        </form>
+      </div>
+    )
+  
     return (
       <div>
-        <h2>blogs</h2>
-        {this.state.blogs.map(blog => 
-          <Blog key={blog._id} blog={blog}/>
-        )}
+        <Notification message={this.state.error} />
+
+        {this.state.user === null ?
+          loginForm() : showBlogs()
+        }
       </div>
     );
   }
