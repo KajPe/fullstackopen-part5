31fb735445b076a69105f1f67975c21260dbfb8d
kaj_pesio <15047180+KajPe@users.noreply.github.com>
2018-10-03 23:16:00 +0300

    Teht 5.13

--- frontend/src/components/SimpleBlog.test.js
+++ frontend/src/components/SimpleBlog.test.js
@@ -3,13 +3,13 @@ import { shallow } from 'enzyme'
 import SimpleBlog from './SimpleBlog'
 
 describe.only('<SimpeBlog />', () => {
-  it('renders content', () => {
-    const blog = {
-      title: 'Life in a shadow',
-      author: 'The big elefant',
-      likes: 79
-    }
+  const blog = {
+    title: 'Life in a shadow',
+    author: 'The big elefant',
+    likes: 79
+  }
 
+  it('renders content', () => {
     const tstComponent = shallow(<SimpleBlog blog={blog} />)
 
     const contentDiv = tstComponent.find('.titleauthor')
@@ -18,5 +18,17 @@ describe.only('<SimpeBlog />', () => {
 
     const likesDiv = tstComponent.find('.likes')
     expect(likesDiv.text()).toContain(blog.likes)
+  })
+
+  it('Click the button twice and check event is called twice', () => {
+    const mockHandler = jest.fn()
+
+    const tstComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler} />)
+
+    const button = tstComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+
+    expect(mockHandler.mock.calls.length).toBe(2)
   })
 })
